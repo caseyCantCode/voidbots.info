928e82fdb91dbde4d4d309040257e9364b575de5
nearlySplat <splatterxl@outlook.ie>
2021-06-04 19:10:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const app = require("express")()
-app.set(`views`, `./src/views`)
-app.get(`/`, (req, res) => res.render(`home.ejs`))
-app.listen(3002, () => console.log(`voidbots.info is online!`))
+app.set("views", "./src/views")
+app.get("/", (req, res) => res.render("home.ejs"))
+app.listen(3002, () => console.log("voidbots.info is online'"))
